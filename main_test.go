b3510f2a70c29f05e1759d38cac744b5905006e0
santiago-rodrig <santo1996.29@gmail.com>
2021-03-06 13:13:27 -0400

    test for RotateInts

--- main_test.go
+++ main_test.go
@@ -12,6 +12,28 @@ func TestReverseInts(t *testing.T) {
 	}
 }
 
+func TestRotateInts(t *testing.T) {
+	t.Run("to the left 2 places", func(t *testing.T) {
+		s := []int{1, 2, 3}
+		want := []int{3, 1, 2}
+		RotateInts(s, 2, false)
+
+		if !equalInts(want, s) {
+			t.Errorf("want %v, got %v", want, s)
+		}
+	})
+
+	t.Run("to the right 2 places", func(t *testing.T) {
+		s := []int{1, 2, 3}
+		want := []int{2, 3, 1}
+		RotateInts(s, 2, true)
+
+		if !equalInts(want, s) {
+			t.Errorf("want %v, got %v", want, s)
+		}
+	})
+}
+
 func equalInts(s1, s2 []int) bool {
 	if len(s1) != len(s1) {
 		return false
